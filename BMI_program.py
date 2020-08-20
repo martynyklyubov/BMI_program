9b92e724d1e73d472b576a86adac3dd890d09f14
Lyubov Martynyk <50379289+martynyklyubov@users.noreply.github.com>
2020-08-20 15:42:18 -0700

    Checking the suspicious input with None return

--- BMI_program.py
+++ BMI_program.py
@@ -1,4 +1,8 @@
-def bmi(weight, height):
+def bmi(weight, height): ## formula to evaluate the Body Mass
+    if height < 1.0 or height > 2.5 or \  
+    weight < 20 or weight > 200: ## checking if the value is trustworthy.
+        return None ## if value is not trustworthy return None
+    
     return weight / height ** 2
 
-print(bmi(52.5, 1.65))
+print(bmi(352.5, 1.65))
