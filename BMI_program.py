6927e8dd134e97763ae73dafe63c8dbc62de6e5a
Lyubov Martynyk <50379289+martynyklyubov@users.noreply.github.com>
2020-08-20 16:28:36 -0700

    Converting ft into m

--- BMI_program.py
+++ BMI_program.py
@@ -1,8 +1,8 @@
 def bmi(weight, height): ## formula to evaluate the Body Mass
-    if height < 1.0 or height > 2.5 or \  
-    weight < 20 or weight > 200: ## checking if the value is trustworthy.
+    if height < 1.0 or height > 2.5 or weight < 20 or weight > 200: ## checking if the value is trustworthy.
         return None ## if value is not trustworthy return None
     
-    return weight / height ** 2
+    return weight / height ** 2 ## convert imperial units to metric ones.
+def ftintom(ft, inch = 0.0):
+    return ft * 0.3048 + inch * 0.0254
 
-print(bmi(352.5, 1.65))
