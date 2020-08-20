20e3b1158566cf7a4248b2b0a2ad4d7ab9cddfd5
Lyubov Martynyk <50379289+martynyklyubov@users.noreply.github.com>
2020-08-20 16:30:35 -0700

    Converting lb into kg

--- BMI_program.py
+++ BMI_program.py
@@ -1,8 +1,16 @@
-def bmi(weight, height): ## formula to evaluate the Body Mass
+def ftintom(ft, inch = 0.0): # convertinf ft into m
+    return ft * 0.3048 + inch * 0.0254
+
+
+def lbstokg(lb): # converting lb into kg
+    return lb * 0.45359237
+
+
+def bmi(weight, height):
     if height < 1.0 or height > 2.5 or weight < 20 or weight > 200: ## checking if the value is trustworthy.
         return None ## if value is not trustworthy return None
     
-    return weight / height ** 2 ## convert imperial units to metric ones.
-def ftintom(ft, inch = 0.0):
-    return ft * 0.3048 + inch * 0.0254
+    return weight / height ** 2 ## formula to evaluate the Body Mass
 
+
+print(bmi(weight = lbstokg(176), height = ftintom(5, 7)))
